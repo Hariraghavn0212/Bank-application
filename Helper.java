d1eb1bce3fb1eb74d205b181bb3bb470062e8cfb
Hariraghavan T <hariraghavan254@gmail.com>
2025-01-25 10:38:43 +0530

    Main.java

--- Helper.java
+++ Helper.java
@@ -1,5 +1,5 @@
-public class Helper {
-    public static void main(String[] args) {
-        Atm.atm();
+public class Main {
+    public static void main(String[] args) throws CloneNotSupportedException {
+        ATM.start();//to call  the start method with the help of atm class
     }
 }
